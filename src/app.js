712946ae43bedda5aa21fb40cf630ad73930acac
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2018-02-28 15:17:56 -0500

    Adding sticky Javascript

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import Sticky from 'Root/sticky';
+
+let addButton = document.getElementById('add-anchor'),
+    addStickyListener = function(evt) {
+        //Send AJAX call to get the stciky dom.
+        var newStickyRequest = new XMLHttpRequest();
+        newStickyRequest.open('GET', 'sticky.html', true);
+        newStickyRequest.responseType = 'text';
+        newStickyRequest.onload = function(e) {
+            if (this.status == 200) {
+                createSticky(this.responseText);
+            }
+        };
+        newStickyRequest.send();
+    };
+
+//Add click event handler/listener
+addButton.addEventListener('click', addStickyListener);
+
+/**
+ * Creates new sticky with the sticky dom string.
+ *
+ * @function createSticky
+ * @param  {string} stickyDom {Sticky dom string}
+ * @public
+ */
+var createSticky = function(stickyDom) {
+    var stickyArea = document.querySelector('.sticky-area'),
+        sticky;
+    sticky = new Sticky(stickyArea);
+    sticky.create();
+}
---
+++ src/sticky.js
@@ -0,0 +1,62 @@
+export default class Sticky {
+
+    /**
+     * Constructor.
+     * @param  {HTMLElement} parent {parent node for the sticky}
+     * @public
+     */
+    constructor(parent) {
+        this.parent = parent;
+        this.position = this.getPosition();
+        this.target = null;
+    }
+
+    /**
+     * Create sticky on the DOM.
+     *
+     * @function {create}
+     * @public
+     */
+    create() {
+        let target = this.target,
+            parent = this.parent,
+            position = this.position;
+        if (!target) {
+            target = document.createElement('div'),
+                target.classList.add('sticky');
+            //Set sticky position
+            target.style.top = position.top;
+            target.style.left = position.left;
+            //Set dom
+            target.innerHTML = stickyDom;
+            //Add new sticky to sticky area
+            parent.appendChild(target);
+        }
+    }
+
+
+    /**
+     * Returns random top and left position with max allowed screen width.
+     *
+     * @function getPosition
+     * @return {Object} {Object with random top & left}
+     * @private
+     */
+    getPosition() {
+        let stickySize = 250, //Width & Height of the sticky
+            topPadding = 20, //Padding around nav bar
+            vPadding = 10, //Padding on both left and right end of the screen
+            navBarHeight = 50, //Height of the nav bar
+            minTop = navBarHeight + topPadding, //Min top position should be nar bar height plus top padding
+            maxScreenWidth = window.innerWidth - (stickySize + 2 * vPadding), //max width should exclude sticky width and padding
+            maxScreenHeight = window.innerHeight - (stickySize + minTop), //max height should exclude sticky height and padding
+            randomLeft = Math.ceil(Math.random() * maxScreenWidth), //Random left with max screen width
+            randomTop = Math.ceil(Math.random() * maxScreenHeight); //Random top with max screen height
+        randomTop = randomTop < minTop ? minTop : randomTop; //If random top is less than min top then use min top
+        randomLeft = randomLeft < vPadding ? vPadding : randomLeft; //If random left is less than vPadding then use vPadding
+        return {
+            top: randomTop,
+            left: randomLeft
+        };
+    }
+}
